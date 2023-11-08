1c17ec075e21716f27e30b4e7ca23a87137929cd
adomasr <adomas.razmus@gmail.com>
2023-11-08 03:06:52 +0000

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+import Navbar from './Components/NavBar/Navbar';
+
+export default function App() {
+    return (
+        <div className='app'>
+            <Navbar />
+        </div>
+    );
+};
