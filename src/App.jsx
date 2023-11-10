63e5b838e58b8c40c284ab0456159977118149af
adomasr <adomas.razmus@gmail.com>
2023-11-10 02:25:53 +0000

    revorked nav and app structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import react from 'react';
 
-import Navbar from './Components/NavBar/Navbar';
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {ThemeProvider} from "styled-components";
 
-export default function App() {
+import './globalStyles/styles.css';
+import Navbar from "./Components/Navbar/Navbar";
+
+const darkTheme = {         //move to a separate file as a styled component
+    bg: '#1C1C27',
+    bgLight: '#1C1E27',
+    primary: '#854CE6',
+    text_primary: '#F2F3F4',
+    text_secondary: '#b1b2b3',
+    card: '#171721',
+    card_light: '#191924',
+    button: '#854CE6',
+    white: '#FFFFFF',
+    black: '#000000'
+};
+
+
+function App() {
     return (
-        <div className='app'>
-            <Navbar />
-        </div>
+        <ThemeProvider theme={darkTheme}>
+            <Router>
+                <Navbar />
+            </Router>
+        </ThemeProvider>
+        
     );
 };
+
+export default App;
